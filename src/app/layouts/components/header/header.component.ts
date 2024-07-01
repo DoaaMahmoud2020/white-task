a058c496264f1077676da5497d9cd31191facc98
DoaaMahmoud2020 <40741671+DoaaMahmoud2020@users.noreply.github.com>
2024-07-01 21:13:27 +0300

    Udpate header design

--- src/app/layouts/components/header/header.component.ts
+++ src/app/layouts/components/header/header.component.ts
@@ -1,14 +1,23 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule,RouterModule],
+  imports: [
+    RouterModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    RouterLink,
+    RouterLinkActive,
+    NgOptimizedImage
+  ],
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
